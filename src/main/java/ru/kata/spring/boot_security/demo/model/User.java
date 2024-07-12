370fb67cf65def31d506e77cacec111b26fc47a5
Noobowski <p.a.kons@yandex.ru>
2024-07-12 22:12:08 +0300

    added lombok

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -49,8 +49,6 @@ public class User /*implements UserDetails*/ {
         this.password = password;
     }
 
-
-
 /*@Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
