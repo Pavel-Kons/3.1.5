fde858dd3f1bd0dc0c82c7d5ae037666f5d5c300
Noobowski <p.a.kons@yandex.ru>
2024-07-13 15:58:46 +0300

    delete comments

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -27,47 +27,4 @@ public class UserController {
     public String getLoginPage() {
         return "redirect:/login";
     }
-
-/*    @GetMapping(value = "/")
-    public String printWelcome(ModelMap model) {
-        return "redirect:users";
-    }
-
-    @GetMapping(value = "/user")
-    public String getAllUsers(ModelMap model,
-                              @RequestParam(value = "count", required = false, defaultValue = "100") Integer count) {
-        model.addAttribute("users", userService.getUsers(count));
-
-        return "/user";
-    }
-
-    @GetMapping("/users/newUser")
-    public String getNewUserPage(@ModelAttribute("user") User user) {
-        return "admin/newUser";
-    }
-
-    @PostMapping("/users/newUser")
-    public String createNewUser(@ModelAttribute("user") User user) {
-        userService.saveUser(user);
-        return "redirect:/users";
-    }
-
-    @GetMapping("/users/editUser")
-    public String getUser(ModelMap model,
-                          @RequestParam(value = "id") Long id) {
-        model.addAttribute("user", userService.getUserById(id));
-        return "admin/editUser";
-    }
-
-    @PatchMapping(value = "/users")
-    public String updateUser(@ModelAttribute("user") User user) {
-        userService.updateUser(user);
-        return "redirect:/users";
-    }
-
-    @DeleteMapping("/users")
-    public String deleteUser(@RequestParam(value = "id", required = false) Long id) {
-        userService.deleteUser(id);
-        return "redirect:user/users";
-    }*/
 }
