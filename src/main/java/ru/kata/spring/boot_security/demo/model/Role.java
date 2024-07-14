1889820169150e61d7df2f27013a8c2d0a553282
Noobowski <p.a.kons@yandex.ru>
2024-07-14 18:42:19 +0300

    update html

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -32,6 +32,11 @@ public class Role implements GrantedAuthority {
     }
 
     @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
     public String getAuthority() {
         return name;
     }
