8a32c9b7f2060a27d7bfcb585d2ca2cc08720d6b
Noobowski <p.a.kons@yandex.ru>
2024-07-21 14:19:25 +0300

    rest controller to basic

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,15 +1,15 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
 
-@RestController
+@Controller
 public class UserController {
     private final UserService userService;
 
@@ -18,15 +18,20 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/")
+    public String getLoginPage() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/admin")
+    public String getAdminPage() {
+        return "/admin";
+    }
+
     @GetMapping(value = "/user")
     public String getUserPage(Principal principal, ModelMap model) {
         User user = userService.findByEmail(principal.getName());
         model.addAttribute(user);
         return "/user";
-    }
-
-    @GetMapping(value = "/")
-    public String getLoginPage() {
-        return "redirect:/login";
     }
 }
